3548443042b743b60b2375e422c3df7213b80121
TonyStark992 <maxime.castillo@gmail.com>
2020-10-09 00:46:33 +0200

    work in progress

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 // APIKEY définie dans apiKey.js
 
-const searchedMovie = "Iron Man"
+//const searchedMovie = "Iron Man"
 
-const URL = `http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchedMovie}`
 
-const myFunction = () =>
-{
-    console.log("Texte de myFunction");
+const search = (e) => {
+    e.preventDefault();
+    const searchedMovie = document.getElementById("searchInput").value;
+    searchMovie(searchedMovie);
 }
 
-const searchMovie = async () => {
-    const response = await fetch(URL);
+const searchMovie = async (searchedMovie) => {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchedMovie}`);
     const data = await response.json();
-    saveData(data);
+    displayData(data.Search);
 }
 
-const saveData = (data) => {
-    data.Search.forEach(e => {
-        title = e.Tile;
-        year = e.year;
-        poster = e.Poster;
+const displayData = (data) => {
+    const moviesDiv = document.getElementById('movies');
+    moviesDiv.innerHTML = "";
+    data.forEach(e => {
+        moviesDiv.innerHTML += 
+        `<div class="card text-center" style="width: 18rem;">
+            <img src=${e.Poster} class="card-img-top" alt="Movie's poster here">
+            <div class="card-body">
+                <h5 class="card-title">${e.Title}</h5>
+                <p class="card-text">${e.Year}</p>
+                <a id="${e.imdbID}" href="#" class="btn btn-primary">Read More</a>
+            </div>
+        </div>`;
+        console.log(e.imdbID);
+        addModal(e);
+        document.getElementById(`${e.imdbID}`).addEventListener("click", displayMore);
     });
+};
+
+const addModal = (e) => {
+    console.log("Fonction ajout modal");
+    console.log("e :", e);
 }
+
+const displayMore = (e) => {
+    //e.preventDefault();
+    console.log("Fonction displayMore");
+}
+
+document.getElementById('submitBtn').addEventListener("click", search);
+
+
+//http://www.omdbapi.com/?apikey=ceec6004&i=tt0112818
