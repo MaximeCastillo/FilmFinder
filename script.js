1a503ce0491a536922bce15b27d358abec4caee9
TonyStark992 <maxime.castillo@gmail.com>
2020-10-09 02:21:07 +0200

    script.js almost finished

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// APIKEY définie dans apiKey.js
-
-//const searchedMovie = "Iron Man"
-
-
 const search = (e) => {
     e.preventDefault();
     const searchedMovie = document.getElementById("searchInput").value;
@@ -15,25 +10,18 @@ const searchMovie = async (searchedMovie) => {
     displayData(data.Search);
 }
 
-const searchPreciseMovie = async (movieID) => {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${movieID}`);
-    const data = await response.json();
-    console.log("Je suis dans la fonction searchPreciseMovie");
-    return data;
-}
-
-const displayData = (data) => {
+const displayData = async (moviesList) => {
     const moviesDiv = document.getElementById('movies');
     moviesDiv.innerHTML = "";
-    data.forEach(e => {
-        const preciseData = searchPreciseMovie(e.imdbID);
-        console.log("Coucou", preciseData.Released);
+    for (const movie of moviesList) {
+        const preciseResponse = await fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${movie.imdbID}`);
+        const preciseMovie = await preciseResponse.json();
         moviesDiv.innerHTML += 
         `<div class="card text-center" style="width: 18rem;">
-            <img src=${e.Poster} class="card-img-top" alt="Movie's poster here">
+            <img src=${movie.Poster} class="card-img-top" alt="Movie's poster here">
             <div class="card-body">
-                <h5 class="card-title">${e.Title}</h5>
-                <p class="card-text">${preciseData.Released}</p>
+                <h5 class="card-title">${movie.Title}</h5>
+                <p class="card-text">${preciseMovie.Released}</p>
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
                     Read More
@@ -44,24 +32,23 @@ const displayData = (data) => {
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">${e.Title}</h5>
+                                <h5 class="modal-title" id="exampleModalLongTitle">${movie.Title}</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div class="modal-body">
-                                ...
+                            ${preciseMovie.Plot}
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>`;
-    });
+    };
 };
 
 document.getElementById('submitBtn').addEventListener("click", search);
